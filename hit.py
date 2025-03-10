a43bf657284eab8662115b9effd91d2795aa6ffa
Serena <swarner26@wooster.edu>
2025-03-10 13:56:50 -0400

    Basically Finished

--- hit.py
+++ hit.py
@@ -1,6 +1,24 @@
 class board:
-    map = [[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "]]
+    map = []
+
+    def __init__(self):
+        for i in range(10):
+            self.map.append([])
+            for j in range(10):
+                self.map[i].append(" ")
 
     def hit(self,x,y):
         if x > 9 or x < 0 or y > 9 or y < 0:
             print("Cordinates out of range")
+            return None
+        
+        self.map[x][y] = "X"
+    
+
+board1 = board()
+print(board.map)
+
+board1.hit(5,5)
+
+print(board.map)
+        
