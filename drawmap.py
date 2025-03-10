cf770d304ad84eac8848f7d90b34755e199884ed
npargas24 <114077181+npargas24@users.noreply.github.com>
2025-03-10 15:11:14 -0400

    added map drawing shtuff

--- drawmap.py
+++ drawmap.py
@@ -1,3 +1,16 @@
 class board:
     def drawmap():
-        
+        pass
+
+
+#whether or not it was hit and is there a ship piece there?
+hit = '0'
+ship = '0'
+space = [hit,ship]
+rows, cols = (10, 10)
+
+#initialize space for each element in col then repeat in with rows
+arr = [[space for i in range(cols)] for j in range(rows)]
+
+for row in arr:
+    print(row)
