38c9f9070c39771be31069ba770f956019229f63
critterday <tjons26@wooster.edu>
2025-03-12 13:45:10 -0400

    draw function works

--- drawmap.py
+++ drawmap.py
@@ -1,17 +1,26 @@
 class board:
-    map = [[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "],[" ", " ", " ", " ", " "]]
+     
+    map = [[" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "]]
 
-    def mapinit(h):
-        if h == True:
-            for i in map:
-                for j in map[i]:
-                    map[i[j]] = "\U00025A1"
-        else:
-            for i in map:
-                for j in map[i]:
-                    map[i[j]] = 
+    def __init__(self, h):
+        for i in range(0,len(self.map)):
+            for j in range(0, len(self.map)):
+                if h == False:
+                    self.map[i][j] = "\U0001f30a"
+                else:
+                    self.map[i][j] = "◼️ "
         
-    
-    def drawmap():
-        
-        
+    def drawmap(self):
+        for i in range(len(self.map)):
+            printstring = ""
+            for j in self.map[i]:
+                printstring = printstring + j
+            print(printstring)
+
+
+hiddenboard = board(True)
+hiddenboard.drawmap()
+
+visibleboard = board(False)
+visibleboard.drawmap()
+
