346e231a0ff8072300ddb6ec1f9ef448ab7882a9
critterday <tjons26@wooster.edu>
2025-03-14 13:32:16 -0400

    basic gameplay loop

---
+++ gameplayloop.py
@@ -0,0 +1,25 @@
+for i in range (0, 10):
+    print("\n")
+print("Welcome to Battleship! \n Press 0 to start game!")
+
+
+def fire(player, shotscount):
+    for i in range (1, shotscount):
+    print("Select an integer X value")
+    x = input()
+    print("Select an integer Y value")
+    y = input():
+    # check if there's a ship at map[x][y]
+        # change map[x][y] to an explosion emoji if there is one
+
+
+if input() == "0":
+    #initialize all four boards
+    for i in range(1, [number of ships]):
+        #player one places a ship
+        #player two places a ship
+    while # both players have ships alive:
+        fire(player1, 5)
+        fire(player2, 5)
+        #update scoreboard
+        
